0ca140c21a2e40f7f320433da26c208d7d29acc7
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-14 09:26:29 +0100

    Creates a function to searchForWord

--- crawl.js
+++ crawl.js
@@ -17,3 +17,11 @@ request(pageToVisit, function(error, response, body) {
     //  console.log("html", $('html'));
    }
 });
+
+function searchForWord($, word) {
+    var bodyText = $('html > body').text();
+    if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+      return true;
+    }
+    return false;
+}
