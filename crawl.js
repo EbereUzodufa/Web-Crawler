bd5a39dc2d7c05bb6f59884633a0a2e992382be3
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-14 09:57:04 +0100

    Create function to get links in url

--- crawl.js
+++ crawl.js
@@ -5,17 +5,18 @@ var URL = require("url-parse");
 var pageToVisit = "http://www.arstechnica.com";
 console.log("Visiting page " + pageToVisit);
 request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-    //  console.log("html", $('html'));
-   }
+    if(error) {
+        console.log("Error: " + error);
+    }
+    // Check status code (200 is HTTP OK)
+    console.log("Status code: " + response.statusCode);
+    if(response.statusCode === 200) {
+        // Parse the document body
+        var $ = cheerio.load(body);
+        console.log("Page title:  " + $('title').text());
+        //console.log("html", $('html'));
+        collectInternalLinks($);
+    }
 });
 
 function searchForWord($, word) {
@@ -24,4 +25,24 @@ function searchForWord($, word) {
       return true;
     }
     return false;
+}
+
+// Getting Hyperlinks that comes as relative paths or absolute paths.
+function collectInternalLinks($) {
+    var allRelativeLinks = [];
+    var allAbsoluteLinks = [];
+
+    var relativeLinks = $("a[href^='/']");
+    relativeLinks.each(function() {
+        allRelativeLinks.push($(this).attr('href'));
+
+    });
+
+    var absoluteLinks = $("a[href^='http']");
+    absoluteLinks.each(function() {
+        allAbsoluteLinks.push($(this).attr('href'));
+    });
+
+    console.log("Found " + allRelativeLinks.length + " relative links");
+    console.log("Found " + allAbsoluteLinks.length + " absolute links");
 }
