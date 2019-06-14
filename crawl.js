e38d7d37aed8b20f1a0f6415bf16545d784d9525
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-14 10:09:21 +0100

    Final version from netinstructions

--- crawl.js
+++ crawl.js
@@ -1,48 +1,71 @@
-var request = require("request");
-var cheerio = require("cheerio");
-var URL = require("url-parse");
+var request = require('request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
 
-var pageToVisit = "http://www.arstechnica.com";
-console.log("Visiting page " + pageToVisit);
-request(pageToVisit, function(error, response, body) {
-    if(error) {
-        console.log("Error: " + error);
-    }
-    // Check status code (200 is HTTP OK)
-    console.log("Status code: " + response.statusCode);
-    if(response.statusCode === 200) {
-        // Parse the document body
-        var $ = cheerio.load(body);
-        console.log("Page title:  " + $('title').text());
-        //console.log("html", $('html'));
-        collectInternalLinks($);
-    }
-});
+var START_URL = "http://www.arstechnica.com";
+var SEARCH_WORD = "stemming";
+var MAX_PAGES_TO_VISIT = 10;
 
-function searchForWord($, word) {
-    var bodyText = $('html > body').text();
-    if(bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
-      return true;
-    }
-    return false;
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+
+pagesToVisit.push(START_URL);
+crawl();
+
+function crawl() {
+  if(numPagesVisited >= MAX_PAGES_TO_VISIT) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  var nextPage = pagesToVisit.pop();
+  if (nextPage in pagesVisited) {
+    // We've already visited this page, so repeat the crawl
+    crawl();
+  } else {
+    // New page we haven't visited
+    visitPage(nextPage, crawl);
+  }
 }
 
-// Getting Hyperlinks that comes as relative paths or absolute paths.
+function visitPage(url, callback) {
+  // Add page to our set
+  pagesVisited[url] = true;
+  numPagesVisited++;
+
+  // Make the request
+  console.log("Visiting page " + url);
+  request(url, function(error, response, body) {
+     // Check status code (200 is HTTP OK)
+     console.log("Status code: " + response.statusCode);
+     if(response.statusCode !== 200) {
+       callback();
+       return;
+     }
+     // Parse the document body
+     var $ = cheerio.load(body);
+     var isWordFound = searchForWord($, SEARCH_WORD);
+     if(isWordFound) {
+       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
+     } else {
+       collectInternalLinks($);
+       // In this short program, our callback is just calling crawl()
+       callback();
+     }
+  });
+}
+
+function searchForWord($, word) {
+  var bodyText = $('html > body').text().toLowerCase();
+  return(bodyText.indexOf(word.toLowerCase()) !== -1);
+}
+
 function collectInternalLinks($) {
-    var allRelativeLinks = [];
-    var allAbsoluteLinks = [];
-
     var relativeLinks = $("a[href^='/']");
+    console.log("Found " + relativeLinks.length + " relative links on page");
     relativeLinks.each(function() {
-        allRelativeLinks.push($(this).attr('href'));
-
+        pagesToVisit.push(baseUrl + $(this).attr('href'));
     });
-
-    var absoluteLinks = $("a[href^='http']");
-    absoluteLinks.each(function() {
-        allAbsoluteLinks.push($(this).attr('href'));
-    });
-
-    console.log("Found " + allRelativeLinks.length + " relative links");
-    console.log("Found " + allAbsoluteLinks.length + " absolute links");
 }
