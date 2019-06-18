88851e104e317c753c1d7c669420aec7982bea08
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-18 09:23:14 +0100

    Basic variables

--- readApiData.js
+++ readApiData.js
@@ -1,1 +1,14 @@
 const fs = require('fs');
+
+//At this time, i'm practicing with a certain API hence my variable will be restricted to the API format
+
+const baseApi = "https://questions.aloc.ng/api/"; //Basic API URL
+let yearApi; //year attached to api to get the right question
+let subjectAPi; //subject attached to api to get the right question
+let questions =[];//Store questions
+
+//Question years
+
+const startYear = 2001;
+const endYear = 2013;
+
