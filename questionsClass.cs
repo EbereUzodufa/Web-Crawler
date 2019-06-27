c8d6fd9293c962eed2b71140786686076ff28953
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-27 15:27:26 +0100

    Creates function to use Newtonsoft.Json Nugget to read json

--- questionsClass.cs
+++ questionsClass.cs
@@ -1,3 +1,6 @@
+//Adding Newtonsoft Json Nuget
+using Newtonsoft.Json
+
 public class Questions
 {
     public int id {get; set;};
@@ -10,4 +13,42 @@ public class Questions
     public DateTime examType {get; set;};
     public DateTime examYear {get; set;};
     public string subject {get; set;};
+}
+
+private string jsonFile = "questions-export.json";
+
+private List<Questions> QuestionsList = new List<Questions>();
+
+//This collects the JSON file and reads it.
+public void LoadJson()
+{
+    try
+    {
+        if(jsonFile){
+            using (StreamReader r = new StreamReader(jsonFile))
+            {
+                string json = r.ReadToEnd();
+                //Assign questions as list
+                QuestionsList = JsonConvert.DeserializeObject<List<Questions>>(json);
+            }
+        } else{
+            //Message no file ohP
+            
+        }
+    }
+    catch (System.Exception)
+    {
+        throw;
+    }
+    
+}
+
+//Loop throw each question
+private void tapEachQuestion()
+{
+    foreach(var question in QuestionsList)
+    {
+        //Write out each question
+        Console.WriteLine(question);
+    }
 }
