76ae4914d69094d6d5340fd9e7e6c61d873ede73
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-06-27 12:54:51 +0100

    Creates a C# class for questions

---
+++ questionsClass.cs
@@ -0,0 +1,13 @@
+public class Questions
+{
+    public int id {get; set;};
+    public string question {get; set;};
+    public string option {get; set;};
+    public string section {get; set;};
+    public string image {get; set;};
+    public string answer {get; set;};
+    public string solution {get; set;};
+    public DateTime examType {get; set;};
+    public DateTime examYear {get; set;};
+    public string subject {get; set;};
+}
